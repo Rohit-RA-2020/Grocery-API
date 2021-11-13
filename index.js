74c360cf6ca3131435f97df6a66a5d3591b8f1e8
Rohit ranjan <ranjanrohit812@gmail.com>
2021-11-14 01:04:35 +0530

    Docs added

--- index.js
+++ index.js
@@ -29,13 +29,62 @@ let myCart = [
     }
 ];
 
+let docs = [
+    {
+        "Endpoint": "/",
+        "method": "GET",
+        "body": null,
+        "description": "Welcome Page of API & docs"
+    },
+    {
+        "Endpoint": "/grocery",
+        "method": "GET",
+        "body": null,
+        "description": "Returns List of all Grocery Available"
+    },
+    {
+        "Endpoint": "/mycart",
+        "method": "GET",
+        "body": null,
+        "description": "Returns List of all Grocery Available in the users cart"
+    },
+    {
+        "Endpoint": "/grocery/:name",
+        "method": "GET",
+        "body": null,
+        "description": "Returns a single Grocery item matching with the param name"
+    },
+    {
+        "Endpoint": "/grocery/add/:name/:pass",
+        "method": "POST",
+        "body" : {
+            "body" : ""
+        },
+        "description" : "Adds data into main Grocery List only if name & pass matched the credentials"
+    },
+    {
+        "Endpoint": "/addtocart",
+        "method": "POST",
+        "body" : {
+            "body" : ""
+        },
+        "description" : "Adds the item into mycart sent in post request if the item is present in the main list"
+    },
+    {
+        "Endpoint": "/cart/:id",
+        "method": "DELETE",
+        "body": null,
+        "description": "Deletes the item from myCart with given index"
+    }
+];
+
 app.use(cors());
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-    res.json('Welcome to the Grocery Store');
+    res.json(docs);
 });
 
 app.get('/grocery', (req, res) => {
